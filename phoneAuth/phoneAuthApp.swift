cdd2b76533f9b9a326e9a7fc11b59a64bc3e0364
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2024-06-29 21:12:40 +0300

    initial commit

---
+++ phoneAuth/phoneAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  phoneAuthApp.swift
+//  phoneAuth
+//
+//  Created by Вадим on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct phoneAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
